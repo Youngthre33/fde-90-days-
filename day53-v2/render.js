28ec4fab149b04ae746fe846fb40b47f10c0ab4c
Youngthre33 <125173329+Youngthre33@users.noreply.github.com>
2026-09-03 16:29:32 +0800

    complete day53 inline ticket editing state

---
+++ day53-v2/render.js
@@ -0,0 +1,146 @@
+const ticketList =
+    document.querySelector("#ticket-list");
+
+const countText =
+    document.querySelector("#ticket-count");
+
+
+function renderTicketCount(tickets){
+
+    countText.textContent =
+        `当前有${tickets.length}个工单`;
+}
+
+
+function renderTicketList(
+    tickets,
+    editingId
+){
+
+    ticketList.textContent = "";
+
+
+    for(const ticket of tickets){
+
+        const item =
+            document.createElement("li");
+
+
+        const editButton =
+            document.createElement("button");
+
+
+        const deleteButton =
+            document.createElement("button");
+
+
+        if(ticket.id === editingId){
+
+            const editInput =
+                document.createElement("input");
+
+            const cancelButton =
+                document.createElement("button");
+
+
+            editInput.value =
+                ticket.title;
+
+
+            editInput.dataset.id =
+                ticket.id;
+
+
+            item.append(editInput);
+
+
+            editButton.textContent =
+                "保存";
+
+
+            editButton.dataset.action =
+                "save";
+
+
+            cancelButton.textContent =
+                "取消";
+
+            cancelButton.dataset.id = 
+                ticket.id;
+
+            cancelButton.dataset.action = 
+                "cancel";
+
+            item.append(
+                cancelButton
+            );
+
+
+
+        }else{
+
+            const titleText =
+                document.createElement("span");
+
+
+            titleText.textContent =
+                `${ticket.title} - ${ticket.status}`;
+
+
+            item.append(titleText);
+
+
+            editButton.textContent =
+                "编辑";
+
+
+            editButton.dataset.action =
+                "edit";
+        }
+
+
+        editButton.dataset.id =
+            ticket.id;
+
+
+        deleteButton.textContent =
+            "删除";
+
+
+        deleteButton.dataset.id =
+            ticket.id;
+
+
+        deleteButton.dataset.action =
+            "delete";
+
+
+        item.append(
+            editButton,
+            deleteButton
+        );
+
+
+        ticketList.append(item);
+    }
+}
+
+
+function render(
+    tickets,
+    editingId
+){
+
+    renderTicketCount(tickets);
+
+
+    renderTicketList(
+        tickets,
+        editingId
+    );
+}
+
+
+export {
+    render
+};
